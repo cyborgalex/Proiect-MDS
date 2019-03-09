94119e8c50c781180f0e7f20deb2d2df486ab33b
alexgavrila <cyborgspeedy@gmail.com>
2019-03-10 01:43:50 +0200

    Posts on first page from database and pagination

--- main.py
+++ main.py
@@ -1,63 +1,100 @@
 
-cybBranch
 #VSCODE pylint throws an error when using SQLAlchemy
 #pylint:disable=E1101
 
-from flask import Flask, render_template, redirect, url_for,flash
-from flask_wtf import FlaskForm 
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
+
+
+from flask import Flask, render_template, redirect,url_for,flash,request
 from flask_sqlalchemy  import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-import random
-
-
+from models import *
+from math import ceil
 
 app=Flask(__name__)
 app.config['SECRET_KEY']="testkey"
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
 db=SQLAlchemy(app)
+
 login_manager=LoginManager()
 login_manager.init_app(app)
 login_manager.login_view='login'
 
-class user(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20),unique=True)
-    email = db.Column(db.String(20),unique=True)
-    password= db.Column(db.String(80))
-class login_form(FlaskForm):
-    username=StringField('username',validators=[InputRequired(),Length(min=4,max=20)])
-    password=PasswordField('password',validators=[InputRequired(),Length(min=8,max=80)])
+
+
+#Function to get the user object from the database so the login manager knows which user to login
+@login_manager.user_loader
+def load_user(user_id):
+    return user.query.filter_by(id=user_id).first()
+
+
+class paginator:
+    def __init__(self, page,per_page,total):
+        self.page=page
+        self.per_page=per_page
+        self.total=total
     
+    @property
+    def pages(self):
+        return int(ceil(float(self.total)/self.per_page))
+
+    @property
+    def has_next(self):
+        return self.page<self.pages
+
+    @property
+    def has_prev(self):
+        return self.page>1
 
 
 
-class register_form(FlaskForm):
-    username=StringField('username',validators=[InputRequired(),Length(min=4,max=20)])
-    password=PasswordField('password',validators=[InputRequired(),Length(min=8,max=80)])
-    email=StringField('email',validators=[Email(),InputRequired(),Length(min=5,max=80)])
-
-
-
-
+@app.route('/index')
 @app.route('/')
 def index():
+    arguments=request.args
 
-    return render_template('index.html',form=login_form())
+    print(request.endpoint)
+    if 'page' in arguments:
+        page_number=int(arguments['page'])-1
+    else:
+        page_number=0
+
+    count=10
+    total=post.query.count()
+    print(total)
+    pag=paginator(page_number+1,count,total)
+
+    posts=post.query.offset(page_number*count).limit(count).all()
+    
+    print(posts)
+    return render_template('index.html',posts=posts,paginator=pag)
 
 @app.route('/login',methods=['GET','POST'])
 def login():
+    print(request.endpoint)
+
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form=login_form()
     if form.validate_on_submit():
         new=user.query.filter_by(username=form.username.data).first()
-        if new:
-            return new.username
-    return render_template('index.html',form=form)
+        if new is not None:
+            #print(new.password)
+            print(check_password_hash(new.password,form.password.data))
+            if check_password_hash(new.password,form.password.data):
+                login_user(new,remember=form.remember.data)
+                return redirect(url_for('index'))
+            else:
+                flash('Username or password is incorect')
+        else:
+            flash('Username or password is incorect')
+    return render_template('login.html',form=form)
 
 @app.route('/register',methods=['GET','POST'])
 def register():
+    if current_user.is_authenticated:
+            return redirect(url_for('index'))
+
     form=register_form()
     if form.validate_on_submit():
         #ret_query=db.session.query(user.id).filter_by(username=form.username.data).scalar() is not None
@@ -69,8 +106,22 @@ def register():
     return render_template('register.html',form=form)
 
 
+@app.route('/post',methods=['GET','POST'])
+@login_required
+def postingFunction():
+    form=post_form()
+    if form.validate_on_submit():
+        new=post(text=form.post.data,user_id=1)
+        db.session.add(new)
+        db.session.commit()
+    return render_template('post.html',form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-master
---
+++ models.py
@@ -0,0 +1,47 @@
+from flask_login import UserMixin
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from main import db
+
+
+#VSCODE pylint throws an error when using SQLAlchemy
+#pylint:disable=E1101
+
+class user(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20),unique=True)
+    email = db.Column(db.String(20),unique=True)
+    password= db.Column(db.String(80))
+    posts=db.relationship('post',backref='user',lazy=True)
+
+
+
+class post(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    text=db.Column(db.Text(),nullable=False)
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+    comments=db.relationship('comment',backref='post',lazy=True)
+
+class comment(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    text=db.Column(db.Text(),nullable=False)
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+    post_id=db.Column(db.Integer,db.ForeignKey('post.id'),nullable=False)
+
+class login_form(FlaskForm):
+    username=StringField('username',validators=[InputRequired()])
+    password=PasswordField('password',validators=[InputRequired()])
+    remember=BooleanField('remember')
+
+
+
+class register_form(FlaskForm):
+    username=StringField('username',validators=[InputRequired(),Length(min=4,max=20)])
+    password=PasswordField('password',validators=[InputRequired(),Length(min=8,max=80)])
+    email=StringField('email',validators=[Email(),InputRequired(),Length(min=5,max=80)])
+
+
+class post_form(FlaskForm):
+    post=StringField('post',validators=[InputRequired()])
+
