382884001d6196e4d731533f360065acdc4a3b0e
alexgavrila <cyborgspeedy@gmail.com>
2019-04-22 17:53:36 +0300

    Modificat pagina de postare, Adaugat pagina profil

---
+++ static/scripts.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+    let lista=$("#taglist");
+    lista.css("display","none");
+    let tagArray=[]
+    $("#list p").click(function(){
+        value=this.innerHTML;
+        let found=tagArray.findIndex(function(e){return e==value});
+        console.log(found);
+        $(this).toggleClass("clicked");
+        if(found==-1)
+            tagArray.push(this.innerHTML);
+        else
+            tagArray.splice(found,1);
+        lista.val(tagArray.join(" "));
+        console.log(tagArray);
+    });
+});
